78a9ed9f82d8a208bf25d061978a1497454e3a88
brian haltom <brianwh87@gmail.com>
2018-07-05 16:31:45 -0700

    pre-bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :avatar])
-		devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :avatar])
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :avatar, :following])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :avatar, :following])
 	end
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   validates :username, presence: true, uniqueness: true
-  
-  has_many :tweets
+	has_many :tweets
 
-  mount_uploader :avatar, AvatarUploader
+	serialize :following, Array
+	
+	mount_uploader :avatar, AvatarUploader
+
 end
