2d91329e9e1d25e41fd0fda62fda9d0228a92863
brian haltom <brianwh87@gmail.com>
2018-07-05 09:46:16 -0700

    corrections

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-  has_many :
+  
+  has_many :tweets
 
   mount_uploader :avatar, AvatarUploader
 end
